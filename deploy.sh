871a1b6f8d28a964d40876d2aac40e4193979b21
webspaceit <webspaceit@users.noreply.github.com>
2026-08-01 20:00:28 +0600

    Only chmod directories during deploy to keep git tree clean

--- deploy.sh
+++ deploy.sh
@@ -42,6 +42,6 @@ php artisan route:cache
 php artisan view:cache
 
 echo "==> Fixing permissions"
-chmod -R 775 storage bootstrap/cache
+find storage bootstrap/cache -type d -exec chmod 775 {} +
 
 echo "Deploy complete."
